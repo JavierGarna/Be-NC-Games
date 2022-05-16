489f3b13ef3a7549473ad680b2ec2c538b814417
Javier <javiergarna.2107@gmail.com>
2022-05-16 15:25:28 +0100

    #4 logic and tests done

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const app = express();
-const { getCategories } = require("./controllers/controllers")
+const { getCategories, getReview } = require("./controllers/controllers")
 
 app.use(express.json());
 
 app.get("/api/categories", getCategories);
 
+app.get("/api/reviews/:review_id", getReview);
+
+app.use((err, req, res, next) => {
+    if (err.code === '42703') {
+        res.status(400).send({ msg: "bad request"})
+    } else {
+        next(err);
+    }
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status).send({ msg: err.msg})
+});
+
 app.use((req, res, next) => {
     res.status(404).send({ msg: "not found"})
-})
+});
 
 app.use((err, req, res, next) => {
     res.status(500).send({ msg: "internal server error"});
--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,7 +1,16 @@
-const { fetchCategories } = require("../models/models")
+const { fetchCategories, fetchReview } = require("../models/models")
 
 exports.getCategories = (req, res, next) => {
     fetchCategories().then((categories) => {
         res.status(200).send({ categories });
     })
 };
+
+exports.getReview = (req, res, next) => {
+    const { review_id } = req.params;
+    fetchReview(review_id).then((review) => {
+        res.status(200).send({ review });
+    }).catch((err) => {
+        next(err);
+    })
+}
